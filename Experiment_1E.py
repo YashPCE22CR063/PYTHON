aa5101e7df3c83fe0d39559d2a322bb8042677ff
YashPCE22CR063 <2022pcecryash063@poornima.org>
2025-04-30 06:47:27 +0530

    Create Experiment_1E.py
    set data types

---
+++ Experiment_1E.py
@@ -0,0 +1,8 @@
+# initializing empty set
+s1 = set()
+
+s1 = set("PoornimaCollegeJaipur")
+print("Set with the use of String: ", s1)
+
+s2 = set(["Poornima", "College", "Jaipur"])
+print("Set with the use of List: ", s2)
